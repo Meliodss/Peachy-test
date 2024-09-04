cde634b41ba3a69d09ec1957a0e701cffe3ebdf8
danLytvynenko <Leon55665@gmail.com>
2024-09-04 07:51:59 +0300

    hotfix: import error fix

--- src/shopify/sections/index.js
+++ src/shopify/sections/index.js
@@ -1,7 +1,7 @@
 import { register, load } from "@shopify/theme-sections";
 import Header from "@/shopify/sections/header";
 import FeaturedProducts from "@/shopify/sections/featured-products";
-import VariantSwatch from "@/shopify/sections/featured-products";
+import VariantSwatch from "@/shopify/snippets/variant-swatch";
 
 register("header", Header);
 register("featured-products", FeaturedProducts);
